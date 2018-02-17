ff7f3236a830cebd6ce7d099e1266c4bd3f6da05
Sajjad Salaria <imsafay@gmail.com>
2018-02-17 21:54:58 +0530

    Update and rename index.html to index.js

---
+++ Digital Clock/index.js
@@ -0,0 +1,40 @@
+// Sajjad Salaria @xorsalaria
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+class TickTock extends React.Component{
+
+    constructor(props){
+      super(props);
+      this.state = {date: new Date()};
+    }
+
+    componentDidMount() {
+      this.timerID = setInterval (
+        () => this.tick(),
+        1000
+      );
+    }
+
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+     tick() {
+       this.setState({
+         date:new Date()
+       });
+     }
+render() {
+      return (
+        <div>
+          <h1>{this.state.date.toLocaleTimeString()}</h1>
+        </div>
+      );
+}
+}
+
+ReactDOM.render(<TickTock />, document.getElementById('root'));
+
